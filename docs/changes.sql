bdd8e8d10e80cfcad7332db3a2b8d63d2c68960f
Juan Pablo Sosa <jpasosa@gmail.com>
2014-08-19 11:33:54 -0300

    cambio en db para que haya soft delete en los accesos

--- docs/changes.sql
+++ docs/changes.sql
@@ -2,6 +2,14 @@
 
 
 
+
+-- 19 de agosto del 2014
+-- agrego campo activo para que se pueda realizar soft delete
+
+ALTER TABLE  `claves` ADD  `activo` TINYINT( 2 ) NOT NULL DEFAULT  '1' COMMENT  '0 significa q está eliminado.'
+
+
+
 -- 14 de Agosto del 2014
 
 ALTER TABLE  `emails` ADD  `descripcion_email` VARCHAR( 300 ) NOT NULL
